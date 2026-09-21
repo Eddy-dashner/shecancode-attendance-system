e0adf92f0ec9d91122502fc77da17bc712e85d8a
Eddy-Dashner <eddynzobarinda910@gmail.com>
2026-09-21 14:47:40 +0200

    fix: fixing auth and cohorts

--- src/main/java/com/shecancode/attendence/ShecancodeAttendanceSystemApplication.java
+++ src/main/java/com/shecancode/attendence/ShecancodeAttendanceSystemApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+//trying to push and solve main auth
 @SpringBootApplication
 @EnableScheduling
 public class ShecancodeAttendanceSystemApplication {
